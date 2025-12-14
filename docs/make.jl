bb90c4289c264ff847bb99ef332d5ba7e81a7ad2
Ross Dempsey <rossd97@gmail.com>
2025-12-14 01:58:17 -0500

    gh-pages setup

--- docs/make.jl
+++ docs/make.jl
@@ -4,11 +4,12 @@ using Kiwi
 makedocs(
     sitename = "Kiwi.jl",
     format = Documenter.HTML(
-        prettyurls = false,
+        prettyurls = get(ENV, "CI", "false") == "true",
+        canonical = "https://srossd.github.io/Kiwi.jl",
         assets = String[],
     ),
     modules = [Kiwi],
-    remotes = nothing,  # Disable remote source links for local build
+    repo = "github.com/srossd/Kiwi.jl.git",
     checkdocs = :none,  # Don't require all docstrings to be documented
     warnonly = [:cross_references, :missing_docs, :example_block],  # Allow warnings
     pages = [
@@ -32,8 +33,8 @@ makedocs(
     ],
 )
 
-# Remove deploydocs for local building
-# deploydocs(
-#     repo = "github.com/srossd/Kiwi.jl.git",
-#     devbranch = "main",
-# )
+deploydocs(
+    repo = "github.com/srossd/Kiwi.jl.git",
+    devbranch = "main",
+    push_preview = true,
+)
